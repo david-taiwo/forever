3d55447493f24376fd865b7aa5417ce79cf0488d
david-taiwo <davidtaiwo003@gmail.com>
2025-05-30 13:56:21 +0100

    created the admin file and installed all neccessary dependencies

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    // host: "192.168.1.135",
-    port: 5173,
-  },
   plugins: [react()],
+  server: {
+    port: 5174,
+  },
 });
